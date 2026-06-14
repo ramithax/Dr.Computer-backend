737ca83eb0787852b7a375c081b813f6f488e6ac
ramitha <ramithamanilka2003@gmail.com>
2026-06-14 20:13:21 +0530

    feat: implement user registration, login, and retrieval functionality

--- controller/usercontroller.js
+++ controller/usercontroller.js
@@ -2,67 +2,113 @@ import User from "../models/user.js"
 import bcrypt from "bcrypt"
 import jwt from "jsonwebtoken"
 
-export async function createuser(req,res){
-    try{
-        const user=await User.findOne({email:req.body.email})
+export async function createuser(req, res) {
+    try {
+        const user = await User.findOne({ email: req.body.email })
 
-        if(user){
-            res.status(400).json({message:"Email already taken"})
+        if (user) {
+            res.status(400).json({ message: "Email already taken" })
             return
         }
-        else{
+        else {
 
-            const passwordhash=bcrypt.hashSync(req.body.password,10)
+            const passwordhash = bcrypt.hashSync(req.body.password, 10)
 
-            const newuser=new User({
-                email:req.body.email,
-                firstname:req.body.firstname,
-                lastname:req.body.lastname,
-                password:passwordhash
+            const newuser = new User({
+                email: req.body.email,
+                firstname: req.body.firstname,
+                lastname: req.body.lastname,
+                password: passwordhash
             })
             await newuser.save()
-            res.status(201).json({message :"Account created successfull"})
+            res.status(201).json({ message: "Account created successfull" })
         }
 
-    }catch(error){
+    } catch (error) {
         res.status(500).json({ message: "Server error" })
     }
 }
 
-export async function loginuser(req,res){
-    try{
-        if(req.body.email == null || req.body.password == null){
-            res.status(400).json({message :"Email and password required"})
+export async function loginuser(req, res) {
+    try {
+        if (req.body.email == null || req.body.password == null) {
+            res.status(400).json({ message: "Email and password required" })
             return
         }
-        const user=await User.findOne({email:req.body.email})
+        const user = await User.findOne({ email: req.body.email })
 
-        if(!user){
-            res.status(404).json({message :"User not found"})
-            return
-        }
-
-        const ismatch=bcrypt.compareSync(req.body.password, user.password)
-
-        if(!ismatch){
-            res.status(401).json({message:"Invalid password"})
+        if (!user) {
+            res.status(404).json({ message: "User not found" })
             return
         }
 
-        const token= jwt.sign({
-            email:user.email,
-            firstname:user.firstname,
-            lastname:user.lastname,
-            isadmin:user.isadmin,
-            isblock:user.isblock,
-            isemailverified:user.isemailverified,
-            image:user.image
-        },process.env.jwt_key)
+        const ismatch = bcrypt.compareSync(req.body.password, user.password)
 
-        res.status(200).json({message : "Login successful",token:token,isadmin:user.isadmin})
+        if (!ismatch) {
+            res.status(401).json({ message: "Invalid password" })
+            return
+        }
+
+        const token = jwt.sign({
+            email: user.email,
+            firstname: user.firstname,
+            lastname: user.lastname,
+            isadmin: user.isadmin,
+            isblock: user.isblock,
+            isemailverified: user.isemailverified,
+            image: user.image
+        }, process.env.jwt_key)
+
+        res.status(200).json({ message: "Login successful", token: token, isadmin: user.isadmin })
     }
-    catch(error){
+    catch (error) {
         console.error("Login error:", error)
-        res.status(500).json({message: "Server error"})
+        res.status(500).json({ message: "Server error" })
     }
+}
+
+export async function getUser(req, res) {
+
+    if (req.user == null) {
+        return res.status(401).json({
+            message: "Unauthorized user"
+        })
+    }
+
+    try {
+
+        const email = req.user.email;
+
+        const user = await User.findOne({ email: email });
+
+        if (user == null) {
+            return res.status(404).json({
+                message: "User not found"
+            })
+        }
+
+        if (user.isblock == true) {
+            return res.status(403).json({
+                message: "You are blocked"
+            })
+        }
+
+        return res.status(200).json({
+            user: {
+                email: user.email,
+                firstname: user.firstname,
+                lastname: user.lastname,
+                isadmin: user.isadmin,
+                isblock: user.isblock,
+                isemailverified: user.isemailverified,
+                image: user.image
+            }
+        })
+
+    } catch (error) {
+        return res.status(500).json({
+            message: "internal server error"
+        })
+    }
+
 }
--- routers/userrouter.js
+++ routers/userrouter.js
@@ -1,9 +1,10 @@
 import express, { Router } from "express";
-import { createuser, loginuser } from "../controller/usercontroller.js";
+import { createuser, loginuser, getUser } from "../controller/usercontroller.js";
 
-const userrouter=new Router()
+const userrouter = new Router()
 
-userrouter.post('/',createuser)
-userrouter.post('/login',loginuser)
+userrouter.post('/', createuser)
+userrouter.post('/login', loginuser)
+userrouter.get("/", getUser)
 
 export default userrouter
